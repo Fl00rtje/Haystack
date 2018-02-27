ffcd6a03ae4aae252b8dd948d70f585aaeaa9fd6
Iris Cedee <iriscedee@gmail.com>
2018-02-27 12:54:46 +0100

    WIP Items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,13 +7,14 @@ class ItemsController < ApplicationController
   end
 
   def new
-     @item = Item.new
+     @item = Item.new()
   end
 
   def create
-    @item = Item.new
+    @item = Item.new(item_params)
+    @item.user = current_user
     if @item.save
-      redirect_to(Item.last)
+      redirect_to(@item)
     else
       render :new
     end
@@ -23,5 +24,10 @@ class ItemsController < ApplicationController
   end
 
   def update
+  end
+
+private
+  def item_params
+    params.require(:item).permit(:name, :description, :category, :shop_location, :price)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+ get 'items/new', to: "items#new"
+  post'items',to: "items#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
